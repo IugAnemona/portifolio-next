d423751913119d9efcd143af79e6ba6923e7f1e9
IugAnemona <guilhermealvescoc@gmail.com>
2023-10-29 13:13:44 -0300

    Adicionando lista de informações dos projetos

---
+++ contents/projects/projectsList.ts
@@ -0,0 +1,14 @@
+export const projectsList = [
+  {
+    title: "Anime Search App",
+    description:
+      "Aplicativo que permite o usuário pesquisar qual o anime de uma imagem específica, somente colocando o link.",
+    img: "/img/animeSearchImg.png",
+  },
+  {
+    title: "ProdukTable",
+    description:
+      "Aplicativo simples, de um CRUD, feito com SQLite, NodeJs e React",
+    img: "/img/produkTableImg.png",
+  },
+];
